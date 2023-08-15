38155c1719ca14ff52941617ad61cf00889fe3a3
pedro.serafim <pedro.serafim@modalgr.com.br>
2023-08-15 14:50:16 -0300

    deploy vercel

--- app/config/server.js
+++ app/config/server.js
@@ -10,7 +10,7 @@ module.exports = () => {
   const Phrases = require("../src/domain/phrases/routes");
   app.use("/phrases", Phrases);
 
-  app.listen(PORT, () => {
+  app.listen(PROCESS.ENV.PORT || PORT, "0.0.0.0", () => {
     console.log(`Servidor rodando na porta ${PORT}`);
   });
 };
